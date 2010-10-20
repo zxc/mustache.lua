e27f5f3dbbb4d754cb8d75a7934a179c6b832f4a
zxc <brian@brian.home>
2010-10-20 01:25:54 -0400

    * refactored individual tag code * added comments and unescaped tags * added pattern to match section tags

--- mustache.lua
+++ mustache.lua
@@ -6,23 +6,32 @@
 
 module(..., package.seeall);
 
-DEBUG = false
---DEBUG = true
+--DEBUG = false
+DEBUG = true
+
+
+--
+-- pattern section
+--
 
 otag = "{{"
 ctag = "}}"
 
 -- tag modifiers: '&'=no escape, '!'=comment, '{'=no escape
 -- not implemented: '='=change delimiter, '>'=partials
-tag_pattern = "(" .. otag .. "%s*([&!{]?)%s*([^}]+)%s*}?" .. ctag .. ")"
+tag_pattern = "(" .. otag .. "%s*([&!{=>]?)%s*([^}]+)%s*}?" .. ctag .. ")"
 
 -- sect modifiers: '#'=open, '/'=close, '^'=invert
-local sect_pattern = "(" .. otag .. ""
+sect_pattern = "(" .. otag .. "%s*[#^]%s*([^}]+)%s*}?" .. ctag ..  "%s*(.*)%s*" .. 
+               otag .. "%s*/%s*[^}]+%s*}?" .. ctag ..")"
+
+
+--
+-- util functions
+--
 
 -- print debug messages
-local function debug(...)
-    if DEBUG then print(...) end
-end
+local function debug(...) if DEBUG then print(...) end end
 
 -- delete from a string, and optionally append to another
 local function eat(amt, src, dest)
@@ -38,17 +47,66 @@ local function trim(s)
     return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
 
+-- escape characters '&', '>' and '<' for HTML
+local function escape(s)
+    return (s:gsub('&', '&amp;'):gsub('>', '&gt;'):gsub('<', '&lt;'))
+end
+
+-- finds a tag in an environment; functions are called
+local function get_variable(tag, env)
+    if type(env[tag]) == 'function' then return env[tag]() end
+    if type(env[tag]) ~= 'nil' then return env[tag] end
+    return ''
+end
+
+
+--
+-- tag rendering functions
+--
+
+function render_normal(tag, env)
+    debug("[debug]", "normal tag found")
+    return escape(get_variable(tag, env))
+end
+
+local function render_comment(tag, env)
+    debug("[debug]", "comment tag found")
+    return ''
+end
+
+local function render_unescaped(tag, env)
+    debug("[debug]", "unescaped tag found")
+    return get_variable(tag, env)
+end
+
+local function render_partial(tag, env)
+    debug("[debug-error]", "partials not implemented")
+    return ''
+end
+
+local function render_change_delim(tag, env)
+    debug("[debug-error]", "change delimiter not implemented")
+    return ''
+end
+
+modifiers = { [""]  = render_normal
+            , ["!"] = render_comment
+            , ["&"] = render_unescaped
+            , ["{"] = render_unescaped
+            , [">"] = render_partial
+            , ["="] = render_change_delim
+}
+
+--
+-- overall rendering functions
+--
+
 -- render single tags
 local function render_tags(template, env)
-    local ret = ""
-
     while true do
         tagstart, tagend, tag, tagmod, tagname = template:find(tag_pattern)
 
-        if tagstart == nil then 
-            ret = ret .. template
-            break 
-        end
+        if tagstart == nil then break end
         tagname = trim(tagname)
 
         debug("[debug-loop]", "tagstart = ", tagstart)
@@ -56,46 +114,52 @@ local function render_tags(template, env)
         debug("[debug-loop]", "tag      = ", "'"..tag.."'")
         debug("[debug-loop]", "tagmod   = ", "'"..tagmod.."'")
         debug("[debug-loop]", "tagname  = ", "'"..tagname.."'")
-
-        template, ret = eat(tagstart, template, ret)
-
         debug("[debug-loop]", "template = ", "'"..template.."'")
-        debug("[debug-loop]", "ret      = ", "'"..ret.."'")
 
-        if tagmod == '' then
-            if env[tagname] then
-                if type(env[tagname]) == 'function' then
-                    ret = ret .. env[tagname]()
-                else
-                    ret = ret .. env[tagname]
-                end
-            end
-        elseif tagmod == '&' or tagmod == '{' then
-            debug("[debug-loop]", "no escape modifier detected")
-        elseif tagmod == '!' then
-            debug("[debug-loop", "comment detected; skipped")
-        elseif tagmod == '=' or tagmod == '>' then
-            debug("[debug-loop", "modifier '" .. tagmod .. "' not implemented")
-        end
+        replacement = modifiers[tagmod](tagname, env)
+        debug("[debug-loop]", "replace  = ", "'"..replacement.."'")
 
-        template = eat(tagend - tagstart + 2, template)
-
+        template = template:gsub(tag, replacement)
         debug("[debug-loop]", "template = ", "'"..template.."'")
-        debug("[debug-loop]", "ret      = ", "'"..ret.."'")
-
     end
-
-    return ret
-
+    
+    return template
 end
 
 local function render_sections(template, env)
-    return template
+    local ret = ""
+
+    while true do
+        tagstart, tagend, tag, tagname, content = template:find(sect_pattern)
+
+        if tagstart == nil then
+            ret = ret .. template
+            break
+        end
+        tagname = trim(tagname)
+
+        debug("[debug-loop]", "tagstart = ", tagstart)
+        debug("[debug-loop]", "tagend   = ", tagend)
+        debug("[debug-loop]", "tag      = ", "'"..tag.."'")
+        debug("[debug-loop]", "tagname  = ", "'"..tagname.."'")
+        debug("[debug-loop]", "content  = ", "'"..content.."'")
+
+        template, ret = eat(tagstart, template, ret)
+        debug("[debug-loop]", "template = ", "'"..template.."'")
+        debug("[debug-loop]", "ret      = ", "'"..ret.."'")
+
+        val = env[tagname]
+        
+
+        break
+    end
+        
+    return ret
 end
 
 function render(template, env)
     local env = env or {}
 
-    template = render_sections(template, env)
+    --template = render_sections(template, env)
     return render_tags(template, env)
 end
